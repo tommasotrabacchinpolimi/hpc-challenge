01ec28310d928db63edc4906187663be51427cc2
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-02-18 18:17:00 +0100

    replaced float with double

--- src/main.cu
+++ src/main.cu
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cuda.h>
 #include <chrono>
-#define GRID_SIZE 3
+#define GRID_SIZE 200
 #define BLOCK_SIZE 1024
 
 
@@ -16,9 +16,9 @@ void check_cuda(const std::string& msg) {
     }
 }
 
-float dot(const float * x, const float * y, size_t size)
+double dot(const double * x, const double * y, size_t size)
 {
-    float result = 0.0;
+    double result = 0.0;
     for(size_t i = 0; i < size; i++)
     {
         result += x[i] * y[i];
@@ -28,7 +28,7 @@ float dot(const float * x, const float * y, size_t size)
 
 
 
-void axpby(float alpha, const float * x, float beta, float * y, size_t size)
+void axpby(double alpha, const double * x, double beta, double * y, size_t size)
 {
     // y = alpha * x + beta * y
 
@@ -40,13 +40,13 @@ void axpby(float alpha, const float * x, float beta, float * y, size_t size)
 
 
 
-void gemv(float alpha, const float * A, const float * x, float beta, float * y, size_t num_rows, size_t num_cols)
+void gemv(double alpha, const double * A, const double * x, double beta, double * y, size_t num_rows, size_t num_cols)
 {
     // y = alpha * A * x + beta * y;
 
     for(size_t r = 0; r < num_rows; r++)
     {
-        float y_val = 0.0;
+        double y_val = 0.0;
         for(size_t c = 0; c < num_cols; c++)
         {
             y_val += alpha * A[r * num_cols + c] * x[c];
@@ -55,8 +55,8 @@ void gemv(float alpha, const float * A, const float * x, float beta, float * y, 
     }
 }
 
-void generate_matrix(size_t n, float** matrix_out) {
-    auto* matrix = new float[n * n];
+void generate_matrix(size_t n, double** matrix_out) {
+    auto* matrix = new double[n * n];
     for(size_t i = 0; i < n * n; i++) {
         matrix[i] = 0.0;
     }
@@ -70,8 +70,8 @@ void generate_matrix(size_t n, float** matrix_out) {
     *matrix_out = matrix;
 }
 
-void generate_rhs(size_t n, float value, float** rhs_out) {
-    auto* rhs = new float[n];
+void generate_rhs(size_t n, double value, double** rhs_out) {
+    auto* rhs = new double[n];
     for(size_t i = 0; i < n; i++) {
         rhs[i] = value;
     }
@@ -81,9 +81,9 @@ void generate_rhs(size_t n, float value, float** rhs_out) {
 
 
 template<int blockSize>
-__device__ void row_column_mult(const float* A, unsigned int row, int size, const float* p, float* Ap) {
-    __shared__ float sArr[blockSize];
-    __shared__ float partial;
+__device__ void row_column_mult(const double* A, unsigned int row, int size, const double* p, double* Ap) {
+    __shared__ double sArr[blockSize];
+    __shared__ double partial;
     int iter_n = 0;
     if(threadIdx.x == 0) {
         partial = 0.0;
@@ -113,7 +113,7 @@ __device__ void row_column_mult(const float* A, unsigned int row, int size, cons
 }
 
 template<int gridSize, int blockSize>
-__global__ void matrix_vector_kernel(const float* A, float* p, float* Ap, int size) {
+__global__ void matrix_vector_kernel(const double* A, double* p, double* Ap, int size) {
     for(unsigned int i = blockIdx.x; i < size; i+=gridSize) {
         row_column_mult<blockSize>(A,i,size,p,Ap);
     }
@@ -121,15 +121,15 @@ __global__ void matrix_vector_kernel(const float* A, float* p, float* Ap, int si
 }
 
 template<int gridSize, int blockSize>
-void matrix_vector_mult(const float* A, float* p, float* Ap, int size, cudaStream_t stream) {
+void matrix_vector_mult(const double* A, double* p, double* Ap, int size, cudaStream_t stream) {
     matrix_vector_kernel<gridSize, blockSize><<<gridSize, blockSize, 0, stream>>>(A, p, Ap, size);
 }
 
 
 template<int blockSize>
-__global__ void sumArray(const float* array, int size, float* result) {
-    __shared__ float sArr[blockSize];
-    __shared__ float partial;
+__global__ void sumArray(const double* array, int size, double* result) {
+    __shared__ double sArr[blockSize];
+    __shared__ double partial;
     int iter_n = 0;
     if(threadIdx.x == 0) {
         partial = 0;
@@ -160,8 +160,8 @@ __global__ void sumArray(const float* array, int size, float* result) {
 
 
 template<int gridSize, int blockSize>
-__global__ void dot_product_kernel(const float* x, const float* y, float* outArray, int size) {
-    __shared__ float sArr[blockSize];
+__global__ void dot_product_kernel(const double* x, const double* y, double* outArray, int size) {
+    __shared__ double sArr[blockSize];
     if(threadIdx.x == 0) {
         outArray[blockIdx.x] = 0.0;
     }
@@ -184,13 +184,13 @@ __global__ void dot_product_kernel(const float* x, const float* y, float* outArr
 }
 
 template<int gridSize, int blockSize>
-void dot_product(const float* x, const float* y, float* outArray, int size, float* result, cudaStream_t stream) {
+void dot_product(const double* x, const double* y, double* outArray, int size, double* result, cudaStream_t stream) {
     dot_product_kernel<gridSize, blockSize><<<gridSize, blockSize, 0, stream>>>(x, y, outArray, size);
     sumArray<blockSize><<<1, blockSize>>>(outArray, gridSize, result);
 }
 
 template<int gridSize, int blockSize>
-__global__ void axpby_kernel(const float* alpha, const float* x, float* y, int size) {
+__global__ void axpby_kernel(const double* alpha, const double* x, double* y, int size) {
     int th_id = threadIdx.x + blockIdx.x * blockSize;
     if(th_id < size) {
         y[th_id] = (*alpha) * x[th_id] + y[th_id];
@@ -199,7 +199,7 @@ __global__ void axpby_kernel(const float* alpha, const float* x, float* y, int s
 
 
 template<int gridSize, int blockSize>
-__global__ void _minus_axpby_kernel(const float* alpha, const float* x, float* y, int size) {
+__global__ void _minus_axpby_kernel(const double* alpha, const double* x, double* y, int size) {
     int th_id = threadIdx.x + blockIdx.x * blockSize;
     if(th_id < size) {
         y[th_id] = -(*alpha) * x[th_id] + y[th_id];
@@ -207,7 +207,7 @@ __global__ void _minus_axpby_kernel(const float* alpha, const float* x, float* y
 }
 
 template<int gridSize, int blockSize>
-__global__ void xpby_kernel( const float* x, float* y, const float* beta, int size) {
+__global__ void xpby_kernel( const double* x, double* y, const double* beta, int size) {
     int th_id = threadIdx.x + blockIdx.x * blockSize;
     if(th_id < size) {
         y[th_id] = (x[th_id] + (*beta) * y[th_id]);
@@ -216,14 +216,14 @@ __global__ void xpby_kernel( const float* x, float* y, const float* beta, int si
 
 
 
-__global__ void divide(float* div1, float* div2, float* result) {
+__global__ void divide(double* div1, double* div2, double* result) {
     if(threadIdx.x == 0) {
         *result = *div1 / *div2;
     }
 
 }
 
-void matrix_vector(float* matrix, float* vector, float* sol, int size) {
+void matrix_vector(double* matrix, double* vector, double* sol, int size) {
     for(int i = 0; i < size; i++) {
         sol[i] = 0;
         for(int j = 0; j < size; j++) {
@@ -233,32 +233,32 @@ void matrix_vector(float* matrix, float* vector, float* sol, int size) {
 }
 
 template<int gridSize, int blockSize>
-void axpby(float* alpha, const float * x, float * y, int size, cudaStream_t stream)
+void axpby(double* alpha, const double * x, double * y, int size, cudaStream_t stream)
 {
     axpby_kernel<gridSize, blockSize><<<gridSize, blockSize, 0, stream>>>(alpha, x, y, size);
 }
 
 template<int gridSize, int blockSize>
-void _minus_axpby(float* alpha, const float * x, float * y, int size, cudaStream_t stream)
+void _minus_axpby(double* alpha, const double * x, double * y, int size, cudaStream_t stream)
 {
     _minus_axpby_kernel<gridSize, blockSize><<<gridSize, blockSize, 0, stream>>>(alpha, x, y, size);
 }
 
 
 template<int gridSize, int blockSize>
-void xpby(const float * x, float * y, const float* beta, int size, cudaStream_t stream)
+void xpby(const double * x, double * y, const double* beta, int size, cudaStream_t stream)
 {
     xpby_kernel<gridSize, blockSize><<<gridSize, blockSize, 0, stream>>>(x, y, beta, size);
 }
 
 
 
-void conjugate_gradients_serial(const float * A, const float * b, float * x, size_t size, int max_iters, float rel_error, long* execution_time)
+void conjugate_gradients_serial(const double * A, const double * b, double * x, size_t size, int max_iters, double rel_error, long* execution_time)
 {
-    float alpha, beta, bb, rr, rr_new;
-    float * r = new float[size];
-    float * p = new float[size];
-    float * Ap = new float[size];
+    double alpha, beta, bb, rr, rr_new;
+    double * r = new double[size];
+    double * p = new double[size];
+    double * Ap = new double[size];
     int num_iters;
 
     for(size_t i = 0; i < size; i++)
@@ -300,39 +300,39 @@ void conjugate_gradients_serial(const float * A, const float * b, float * x, siz
     }
 }
 
-void conjugate_gradients(const float * A, const float * b, float * x, size_t size, int max_iters, float rel_error, long* execution_time) {
-    float* r_cuda;
-    float* p_cuda;
-    float* Ap_cuda;
-    float *alpha;
-    float *beta;
-    float* bb;
-    float bb_cpu;
-    float* rr;
-    float* rr_new;
-    float* dot_product_out_array;
-    float err;
-    cudaMalloc(&r_cuda, size*sizeof(float));
-    cudaMalloc(&p_cuda, size*sizeof(float));
-    cudaMalloc(&Ap_cuda, size*sizeof(float));
-    cudaMalloc(&dot_product_out_array, sizeof(float)*GRID_SIZE);
-    cudaMalloc(&alpha, sizeof(float));
-    cudaMalloc(&beta, sizeof(float));
-    cudaMalloc(&bb, sizeof(float));
-    cudaMalloc(&rr, sizeof(float));
-    cudaMalloc(&rr_new, sizeof(float));
-    cudaMemcpy(r_cuda, b, size*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(p_cuda, b, size*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemset(x,0,sizeof(float) * size);
+void conjugate_gradients(const double * A, const double * b, double * x, size_t size, int max_iters, double rel_error, long* execution_time) {
+    double* r_cuda;
+    double* p_cuda;
+    double* Ap_cuda;
+    double *alpha;
+    double *beta;
+    double* bb;
+    double bb_cpu;
+    double* rr;
+    double* rr_new;
+    double* dot_product_out_array;
+    double err;
+    cudaMalloc(&r_cuda, size*sizeof(double));
+    cudaMalloc(&p_cuda, size*sizeof(double));
+    cudaMalloc(&Ap_cuda, size*sizeof(double));
+    cudaMalloc(&dot_product_out_array, sizeof(double)*GRID_SIZE);
+    cudaMalloc(&alpha, sizeof(double));
+    cudaMalloc(&beta, sizeof(double));
+    cudaMalloc(&bb, sizeof(double));
+    cudaMalloc(&rr, sizeof(double));
+    cudaMalloc(&rr_new, sizeof(double));
+    cudaMemcpy(r_cuda, b, size*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(p_cuda, b, size*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemset(x,0,sizeof(double) * size);
     int niters;
     cudaStream_t stream1;
     cudaStream_t stream2;
     cudaStreamCreate(&stream1);
     cudaStreamCreate(&stream2);
     dot_product<GRID_SIZE, BLOCK_SIZE>(b, b, dot_product_out_array, (int) size, bb, stream1);
-    cudaMemcpy(&bb_cpu, bb, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&bb_cpu, bb, sizeof(double), cudaMemcpyDeviceToHost);
     err = bb_cpu;
-    cudaMemcpy(rr, bb, sizeof(float), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(rr, bb, sizeof(double), cudaMemcpyDeviceToDevice);
     auto start = std::chrono::high_resolution_clock::now();
     for(niters = 1; niters < max_iters; niters++) {
         matrix_vector_mult<GRID_SIZE, BLOCK_SIZE>(A, p_cuda, Ap_cuda, (int)size, stream1);
@@ -342,8 +342,8 @@ void conjugate_gradients(const float * A, const float * b, float * x, size_t siz
         _minus_axpby<GRID_SIZE, BLOCK_SIZE>(alpha, Ap_cuda, r_cuda, (int) size, stream1);
         dot_product<GRID_SIZE, BLOCK_SIZE>(r_cuda, r_cuda, dot_product_out_array, (int)size, rr_new, stream1);
         divide<<<1, 1, 0, stream1>>>(rr_new, rr, beta);
-        cudaMemcpy(rr, rr_new, sizeof(float), cudaMemcpyDeviceToDevice);
-        cudaMemcpy(&err, rr, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(rr, rr_new, sizeof(double), cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&err, rr, sizeof(double), cudaMemcpyDeviceToHost);
         if(std::sqrt(err / bb_cpu) < rel_error) { break; }
         xpby<GRID_SIZE, BLOCK_SIZE>(r_cuda, p_cuda, beta,  (int)size, stream1);
     }
@@ -375,7 +375,7 @@ int main(int argc, char ** argv) {
 
     int size = 500;
     int max_iters = 1000;
-    float rel_error = 1e-9;
+    double rel_error = 1e-9;
     int serial_trials = 1;
     int parallel_trials = 1;
     if(argc > 1) size = atoi(argv[1]);
@@ -397,60 +397,60 @@ int main(int argc, char ** argv) {
 
     int* size_cuda;
     int* max_iters_cuda;
-    float* tol_cuda;
-    float* matrix;
-    float* matrix_cuda;
-    float* rhs;
-    float* rhs_cuda;
-    float* r_cuda;
-    float* p_cuda;
-    float* Ap_cuda;
+    double* tol_cuda;
+    double* matrix;
+    double* matrix_cuda;
+    double* rhs;
+    double* rhs_cuda;
+    double* r_cuda;
+    double* p_cuda;
+    double* Ap_cuda;
     generate_matrix(size, &matrix);
     generate_rhs(size, 2.0, &rhs);
-    auto* sol = new float[size];
-    float* sol_cuda;
+    auto* sol = new double[size];
+    double* sol_cuda;
 
     for(int i = 0; i < size; i++) {
         sol[i] = 1.0;
     }
 
-    cudaMalloc(&matrix_cuda, size*size*sizeof(float));
-    cudaMalloc(&rhs_cuda, size*sizeof(float));
-    cudaMalloc(&sol_cuda, size*sizeof(float));
+    cudaMalloc(&matrix_cuda, size*size*sizeof(double));
+    cudaMalloc(&rhs_cuda, size*sizeof(double));
+    cudaMalloc(&sol_cuda, size*sizeof(double));
     cudaMalloc(&max_iters_cuda, sizeof(int));
     cudaMalloc(&size_cuda, sizeof(int));
-    cudaMalloc(&tol_cuda, sizeof(float));
-    cudaMalloc(&r_cuda, size*sizeof(float));
-    cudaMalloc(&p_cuda, size*sizeof(float));
-    cudaMalloc(&Ap_cuda, size*sizeof(float));
-    cudaMemcpy(matrix_cuda, matrix, size*size*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(rhs_cuda, rhs, size*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(sol_cuda, sol, size*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc(&tol_cuda, sizeof(double));
+    cudaMalloc(&r_cuda, size*sizeof(double));
+    cudaMalloc(&p_cuda, size*sizeof(double));
+    cudaMalloc(&Ap_cuda, size*sizeof(double));
+    cudaMemcpy(matrix_cuda, matrix, size*size*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(rhs_cuda, rhs, size*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(sol_cuda, sol, size*sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(max_iters_cuda, &max_iters, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(size_cuda, &size, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(tol_cuda, &rel_error, sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(r_cuda, rhs, size*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(p_cuda, rhs, size*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(tol_cuda, &rel_error, sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(r_cuda, rhs, size*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(p_cuda, rhs, size*sizeof(double), cudaMemcpyHostToDevice);
 
 
     for(int i = 0; i < serial_trials; i++) {
         long tmp;
         conjugate_gradients_serial(matrix, rhs, sol, size, max_iters, rel_error, &tmp);
         serial_execution_time += tmp;
-        memset(sol, 0, sizeof(float) * size);
+        memset(sol, 0, sizeof(double) * size);
     }
     for(int i = 0; i < parallel_trials; i++) {
         long tmp;
         conjugate_gradients(matrix_cuda, rhs_cuda, sol_cuda, size, max_iters, rel_error, &tmp);
         parallel_execution_time += tmp;
-        memset(sol, 0, sizeof(float) * size);
+        memset(sol, 0, sizeof(double) * size);
     }
 
     std::cout << "check" << std::endl;
     check_cuda("error");
-    std::cout << "Serial average execution time: " << (float)serial_execution_time/serial_trials << std::endl;
-    std::cout << "Parallel average execution time: " << (float)parallel_execution_time/parallel_trials << std::endl;
-    std::cout << "Speedup: " << (float)((float)serial_execution_time/serial_trials)/((float)parallel_execution_time/parallel_trials) << std::endl;
+    std::cout << "Serial average execution time: " << (double)serial_execution_time/serial_trials << std::endl;
+    std::cout << "Parallel average execution time: " << (double)parallel_execution_time/parallel_trials << std::endl;
+    std::cout << "Speedup: " << (double)((double)serial_execution_time/serial_trials)/((double)parallel_execution_time/parallel_trials) << std::endl;
     printf("Finished successfully\n");
 
 
