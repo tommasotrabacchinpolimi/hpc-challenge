2a3403966303d5d542c80ad669c2c83b4a542d7c
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-02-28 22:59:49 +0100

    fixed a bug

--- src/fpga/main_fpga.cpp
+++ src/fpga/main_fpga.cpp
@@ -228,6 +228,12 @@ void conjugate_gradients(const double * host_A, const double * host_b, double * 
     }
     double res_err = 0;
     for(int i = 0; i < size; i++) {
+        if(std::isnan(tmp[i])) {
+            std::cout << "error nan tmp2" << std::endl;
+        }
+        if(std::isnan(host_b[i])) {
+            std::cout << "error nan tmp2" << std::endl;
+        }
         res_err += (host_b[i] - tmp[i])*(host_b[i] - tmp[i]);
     }
     std::cout << "error: " << res_err << std::endl;
