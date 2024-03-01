8e964e06ca755279f7bf0634c1babc255968a101
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-01 13:13:02 +0100

    fixed a bug

--- src/fpga/multi_fpga.cpp
+++ src/fpga/multi_fpga.cpp
@@ -289,7 +289,7 @@ cl_kernel create_kernel(cl_program program, const char* kernel_name, cl_int* err
 
 
 int main() {
-    size_t size = 500;
+    size_t size = 30;
     int max_iters = 1;
     double tol = 1e-12;
     cl_int err = 0;
