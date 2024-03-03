08044f0888a669e64d4f99b70f7264ca7b87aeae
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-03 12:09:29 +0100

    fixed a bug

--- src/heterogeneous/MainNode.h
+++ src/heterogeneous/MainNode.h
@@ -49,6 +49,7 @@ public:
         for(int i = 2; i < world_size; i++) {
             offset[i] = offset[i-1] + std::min(size/total_device_number*world_device_number[i], max_size[i]);
             partial_size[i-1] = std::min(size/total_device_number*world_device_number[i], max_size[i]);
+            std::cout << "rank " << i << "has been assigned from " << offset[i] << " to " << offset[i] + partial_size[i] << std::endl;
         }
         partial_size[world_size - 1] = size - offset[world_size - 1];
 
