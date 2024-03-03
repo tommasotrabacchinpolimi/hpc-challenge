4d08ac722558824d3af05d2cda0f37cbd3e9058a
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-03 14:11:35 +0100

    added script to run multi fpga on multi nodes

---
+++ multi_fpga_node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --qos=default                      # SLURM qos
+#SBATCH --nodes=2                          # number of nodes
+#SBATCH --ntasks=1                         # number of tasks
+#SBATCH --ntasks-per-node=1                # number of tasks per node
+#SBATCH --time=00:05:00                    # time (HH:MM:SS)
+#SBATCH --partition=fpga                   # partition
+#SBATCH --account=p200301                  # project account
+#SBATCH --cpus-per-task=8                  # CORES per task
+
+module load ifpgasdk && module load 520nmx && module load CMake && module load intel && module load deploy/EasyBuild
+cd build
+git pull
+make main_node
+srun main_node
