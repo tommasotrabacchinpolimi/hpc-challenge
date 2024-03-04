3837d2cfc914f3a09e1c22cd81a92de623b443ca
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-04 16:07:02 +0100

    fixed a bug

--- src/heterogeneous/MainNode.h
+++ src/heterogeneous/MainNode.h
@@ -217,7 +217,7 @@ private:
         is.open(matrix_file_path, std::ios::binary);
         is.read((char*)&buff, sizeof(int));
         is.read((char*)matrix, size * partial_size[0] * sizeof(double));
-        check_matrix(matrix, partial_size[0]);
+        check_matrix(matrix, partial_size[0], 0);
 
         for(int i = 1; i < world_size; i++) {
             is.read((char*)matrix_, size * partial_size[i] * sizeof(double));
