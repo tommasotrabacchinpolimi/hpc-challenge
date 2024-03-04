96022dd765621c200df65ded49ab519b559fdf07
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-04 08:11:57 +0100

    fixed a bug

--- src/heterogeneous/main.cpp
+++ src/heterogeneous/main.cpp
@@ -13,6 +13,7 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if(rank == 0) {
         MainNode<FPGAMatrixVectorMultiplier> mainNode("", "", 100, 1e-12);
+        mainNode.init();
         mainNode.handshake();
         mainNode.compute_conjugate_gradient();
     } else {
