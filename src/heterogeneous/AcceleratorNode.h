b668d3e88b899f3d1f4a4ed60f78d1d219c3d37b
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-07 19:05:45 +0100

    implemented mpi version

--- src/heterogeneous/AcceleratorNode.h
+++ src/heterogeneous/AcceleratorNode.h
@@ -18,23 +18,18 @@ public:
         MPI_Type_commit(&matrixDataType);
         MPI_Scatter(NULL, 0, matrixDataType, &matrixData, 1, matrixDataType, 0, MPI_COMM_WORLD);
         matrix = new double[size * matrixData.partial_size];
-        std::cout << "check1" << std::endl;
         #pragma omp parallel for default(none)
         for(int i = 0; i < size * matrixData.partial_size; i++) {
             matrix[i] = 0.0;
         }
-        std::cout << "check2" << std::endl;
 
         MPI_Recv(matrix, size * matrixData.partial_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        std::cout << "check3" << std::endl;
 
         accelerator.setSize(size);
         accelerator.setPartialSize(matrixData.partial_size);
         accelerator.setMatrix(matrix);
-        std::cout << "check4, partial_size = " << matrixData.partial_size << " size =  "<<size << std::endl;
 
         accelerator.setup();
-        std::cout << "setup completed" << std::endl;
 
     }
 
@@ -51,16 +46,12 @@ public:
 
         while(true) {
 
-            std::cout << "new cycle" << std::endl;
 
             MPI_Bcast(p, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-            std::cout << "broadcast completed" << std::endl;
 
             accelerator.compute(p, Ap);
-            std::cout << "compute completed" << std::endl;
-
+            std::cout << matrixData.partial_size << std::endl;
             MPI_Gatherv(Ap, matrixData.partial_size, MPI_DOUBLE, NULL, NULL, NULL, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-            std::cout << "gather completed" << std::endl;
 
         }
     }
