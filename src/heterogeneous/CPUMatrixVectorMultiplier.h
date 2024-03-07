b3b6dbc25f7a68d54ba52d2c96ea102b5a10fb15
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-07 17:30:14 +0100

    implemented mpi version

---
+++ src/heterogeneous/CPUMatrixVectorMultiplier.h
@@ -0,0 +1,56 @@
+//
+// Created by tomma on 07/03/2024.
+//
+
+#ifndef MATRIX_VECTOR_MULTIPLICATION_CPUMATRIXVECTORMULTIPLIER_H
+#define MATRIX_VECTOR_MULTIPLICATION_CPUMATRIXVECTORMULTIPLIER_H
+
+#include "MatrixVectorMultiplier.h"
+class CPUMatrixVectorMultiplier : public MatrixVectorMultiplier{
+public:
+    virtual void init() {
+
+    };
+
+    virtual void setup() {
+
+    };
+
+    virtual void compute(double* p, double* Ap) {
+#pragma omp for simd nowait
+        for (size_t i = 0; i < partial_size; i += 1) {
+            Ap[i] = 0.0;
+#pragma omp simd
+            for (size_t j = 0; j < size; j++) {
+                Ap[i] += A[i * size + j] * p[j];
+            }
+        }
+    };
+
+    //size is the size of the size x size matrix
+    virtual void setSize(size_t size_) {
+        size = size_;
+    };
+
+    //size_ is the number of rows that are offloaded to the accelerator
+    virtual void setPartialSize(size_t size_) {
+        partial_size = size;
+    };
+
+
+    virtual void setMatrix(double* matrix_) {
+        A = matrix_;
+    };
+
+    virtual ~CPUMatrixVectorMultiplier() = default;
+
+private:
+
+    double* A;
+    size_t size;
+    size_t partial_size;
+
+};
+
+
+#endif //MATRIX_VECTOR_MULTIPLICATION_CPUMATRIXVECTORMULTIPLIER_H
