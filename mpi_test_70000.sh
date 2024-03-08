c01f8fe7779873ba8e82c220e56e460b7c8b8652
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-08 08:19:57 +0100

    added omp parallelization to mpi and fpga version

--- mpi_test_70000.sh
+++ mpi_test_70000.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --qos=default                      # SLURM qos
-#SBATCH --nodes=5                         # number of nodes
-#SBATCH --ntasks=5                         # number of tasks
+#SBATCH --nodes=1                          # number of nodes
+#SBATCH --ntasks=1                         # number of tasks
 #SBATCH --ntasks-per-node=1                # number of tasks per node
-#SBATCH --time=10:00:00                    # time (HH:MM:SS)
+#SBATCH --time=02:00:00                    # time (HH:MM:SS)
 #SBATCH --partition=cpu                    # partition
 #SBATCH --account=p200301                  # project account
 #SBATCH --cpus-per-task=256                # CORES per task
