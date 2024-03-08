268a6362878624d46258cc1db1ca6a725f8fe074
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-08 10:33:36 +0100

    added communication overhead profiling

---
+++ mpi_test_20000_10.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --qos=default                      # SLURM qos
+#SBATCH --nodes=10                         # number of nodes
+#SBATCH --ntasks=10                         # number of tasks
+#SBATCH --ntasks-per-node=1                # number of tasks per node
+#SBATCH --time=00:20:00                    # time (HH:MM:SS)
+#SBATCH --partition=cpu                    # partition
+#SBATCH --account=p200301                  # project account
+#SBATCH --cpus-per-task=256                # CORES per task
+
+module load ifpgasdk && module load 520nmx && module load CMake && module load intel && module load deploy/EasyBuild
+cd build
+make mpi
+
+srun mpi /project/home/p200301/tests/matrix20000.bin /project/home/p200301/tests/rhs20000.bin output_mpi.bin 20000 1e-16
