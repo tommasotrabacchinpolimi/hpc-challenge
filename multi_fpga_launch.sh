d3ec1ed5ffac1905d190723e14f0b47c3c9d9577
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-09 13:10:20 +0100

    improved fpga version

--- multi_fpga_launch.sh
+++ multi_fpga_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --qos=default                      # SLURM qos
-#SBATCH --nodes=5                          # number of nodes
-#SBATCH --ntasks=5                        # number of tasks
+#SBATCH --nodes=10                        # number of nodes
+#SBATCH --ntasks=10                        # number of tasks
 #SBATCH --ntasks-per-node=1                # number of tasks per node
 #SBATCH --time=00:30:00                    # time (HH:MM:SS)
 #SBATCH --partition=fpga                   # partition
@@ -12,8 +12,8 @@ module load ifpgasdk && module load 520nmx && module load CMake && module load i
 cd build
 git pull
 make multi_node
-#srun multi_node /project/home/p200301/tests/matrix10000.bin /project/home/p200301/tests/rhs10000.bin output_mpi.bin 10000 1e-16
-#srun multi_node /project/home/p200301/tests/matrix20000.bin /project/home/p200301/tests/rhs20000.bin output_mpi.bin 20000 1e-16
+srun multi_node /project/home/p200301/tests/matrix10000.bin /project/home/p200301/tests/rhs10000.bin output_mpi.bin 10000 1e-16
+srun multi_node /project/home/p200301/tests/matrix20000.bin /project/home/p200301/tests/rhs20000.bin output_mpi.bin 20000 1e-16
 srun multi_node /project/home/p200301/tests/matrix30000.bin /project/home/p200301/tests/rhs30000.bin output_mpi.bin 30000 1e-16
 srun multi_node /project/home/p200301/tests/matrix40000.bin /project/home/p200301/tests/rhs40000.bin output_mpi.bin 40000 1e-16
 srun multi_node /project/home/p200301/tests/matrix50000.bin /project/home/p200301/tests/rhs50000.bin output_mpi.bin 50000 1e-16
