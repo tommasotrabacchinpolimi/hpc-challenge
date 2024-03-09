d9395036de52aa91f36383a89b2e1ac8150dbf91
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-03-09 12:28:06 +0100

    improved fpga version

--- multi_fpga_launch.sh
+++ multi_fpga_launch.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=00:30:00                    # time (HH:MM:SS)
 #SBATCH --partition=fpga                   # partition
 #SBATCH --account=p200301                  # project account
-#SBATCH --cpus-per-task=200                  # CORES per task
+#SBATCH --cpus-per-task=100                  # CORES per task
 
 module load ifpgasdk && module load 520nmx && module load CMake && module load intel && module load deploy/EasyBuild
 cd build
